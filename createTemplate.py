7af7df82eb5460eef1bbd0b0fd811b9b0ef11d7e
vinovator <vinovator@yahoo.co.in>
2015-12-04 10:28:27 +0000

    Update createTemplate.py
    Added comments

--- createTemplate.py
+++ createTemplate.py
@@ -1,3 +1,14 @@
+# Python 2.76
+# createTemplate.py
+
+"""
+This program calls the  powerplay library I created based on Python-pptx library
+Create a template based on an existing powerpoint file
+Template will be a copy of the source and adds a slide for each layout present in source
+TO:DO - Find way to delete slides from source so that template is a bare skeleton
+Added issue #186 to scanny/python-pptx repository
+"""
+
 from powerPlay import generateTemplate
 
 template = generateTemplate("Infosys_Template.pptx", "My Analysis.pptx")
